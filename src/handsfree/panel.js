a5e4733a06d675bfca046576c60ef41360d298e8
Oz Ramos <midiblocks@gmail.com>
2020-12-26 12:59:59 -0800

    #1 Adds rotation/translation sliders and makes them work

--- src/handsfree/panel.js
+++ src/handsfree/panel.js
@@ -1,3 +1,14 @@
+/**
+ * Cache elements and settings
+ */
+const $handsfree = {
+  $rotMult: document.querySelector('#handfree-head-rotation-multiplier'),
+  $transMult: document.querySelector('#handfree-head-translation-multiplier')
+}
+
+/**
+ * Listeners
+ */
 port.onMessage.addListener(message => {
   /**
    * Handle data
@@ -9,13 +20,13 @@ port.onMessage.addListener(message => {
       const node = assetNodes[DEVICE.HEADSET]
       if (!node || !message?.data?.weboji?.rotation) return
 
-      assetNodes[DEVICE.HEADSET].rotation.x = message.data.weboji.rotation[0]
-      assetNodes[DEVICE.HEADSET].rotation.y = message.data.weboji.rotation[1]
-      assetNodes[DEVICE.HEADSET].rotation.z = message.data.weboji.rotation[2]
+      assetNodes[DEVICE.HEADSET].rotation.x = message.data.weboji.rotation[0] * $handsfree.$rotMult.value
+      assetNodes[DEVICE.HEADSET].rotation.y = message.data.weboji.rotation[1] * $handsfree.$rotMult.value
+      assetNodes[DEVICE.HEADSET].rotation.z = message.data.weboji.rotation[2] * $handsfree.$rotMult.value
 
-      assetNodes[DEVICE.HEADSET].position.x = message.data.weboji.translation[0] * 2
-      assetNodes[DEVICE.HEADSET].position.y = message.data.weboji.translation[1] * 2
-      assetNodes[DEVICE.HEADSET].position.z = message.data.weboji.translation[2] * -3
+      assetNodes[DEVICE.HEADSET].position.x = message.data.weboji.translation[0] * 2 * $handsfree.$transMult.value
+      assetNodes[DEVICE.HEADSET].position.y = message.data.weboji.translation[1] * 2 * $handsfree.$transMult.value
+      assetNodes[DEVICE.HEADSET].position.z = message.data.weboji.translation[2] * -3 * $handsfree.$transMult.value
 
       // Update everything. The Polyfill will handle the rest
       updateHeadsetPropertyComponent()
@@ -26,15 +37,18 @@ port.onMessage.addListener(message => {
 })
 
 /**
- * Initialize Handsfree.js
+ * Inject Handsfree.js into DOM
  */
 document.querySelector('#startHandsfree').addEventListener('click', function () {
   document.querySelector('#startHandsfree').style.display = 'none'
   document.querySelector('#stopHandsfree').style.display = 'inline-block'
-  chrome.runtime.sendMessage({action: 'handsfree-inject'}, function(response) {})
+  chrome.runtime.sendMessage({action: 'handsfree-inject'})
 })
 
+/**
+ * Stop the camera (will reload page)
+ */
 document.querySelector('#stopHandsfree').addEventListener('click', function () {
-  chrome.runtime.sendMessage({action: 'handsfree-reload'}, function(response) {})
+  chrome.runtime.sendMessage({action: 'handsfree-reload'})
   window.location.reload()
 })
